cc5bfa7cc2b1d32ae7af5a00fbc1ba75c516be59
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-05 10:49:18 -0300

    Saving a new department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -10,5 +10,15 @@ public class Utils {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 		
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+			
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 }
